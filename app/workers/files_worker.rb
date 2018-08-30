57265f540408dd07da1de917238cb740d3fbb34a
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-30 15:58:32 +0000

    automatically backed up by learn

--- app/workers/files_worker.rb
+++ app/workers/files_worker.rb
@@ -3,7 +3,7 @@ class FilesWorker
   include Sidekiq::Worker
 
   def perform(files_file)
-    CSV.foreach(files_file, headers: true) do |lead|
+    CSV.foreach(files_file, headers: true) do |song|
       Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
   end
