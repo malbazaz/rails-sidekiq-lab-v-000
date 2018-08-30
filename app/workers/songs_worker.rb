827843f9148b55b575437371145a5045320a9226
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-30 15:59:15 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -2,7 +2,7 @@ class SongsWorker
   require 'csv'
   include Sidekiq::Worker
 
-  def perform(files_file)
+  def perform(songs_file)
     CSV.foreach(files_file, headers: true) do |song|
       Song.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
